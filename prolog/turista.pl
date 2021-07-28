5ba12c5182550f8dbc9215e1fc519b669f4009c9
ThElement12 <20170861@ce.pucmm.edu.do>
2021-07-28 11:05:03 -0400

    Se agregan algunas reglas pl

---
+++ prolog/turista.pl
@@ -0,0 +1,26 @@
+%lugares disponibles para hacer turismo
+lugares(puerto_plata).
+lugares(santiago).
+lugares(santodomingo).
+lugares(punta_cana).
+
+%lista de actividades
+actividad(teatro).
+actividad(restaurante1).
+actividad(restaurante2).
+actividad(restaurante3).
+actividad(cine).
+actividad(skydiving).
+actividad(montar_coche).
+
+%Actividades con su respectivo precio
+actividad_precio(teatro, 850).
+actividad_precio(restaurante1, 650).
+actividad_precio(restaurante2, 550).
+actividad_precio(restaurante3, 750).
+actividad_precio(discoteca1, 900).
+actividad_precio(discoteca2, 750).
+actividad_precio(casino, 2000).
+actividad_precio(cine, 350).
+actividad_precio(skydiving, 1000).
+actividad_precio(montar_coche, 100).
