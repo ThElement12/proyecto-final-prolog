adf1c9ba7bb69d29a8000df474ab7848429e7c50
ThElement12 <20170861@ce.pucmm.edu.do>
2021-07-29 19:14:54 -0400

    Se arreglan las reglas

--- src/prolog/turista.pl
+++ src/prolog/turista.pl
@@ -17,30 +17,49 @@ sectores(santo_domingo, [boca_chica, centro_santo_domingo]).
 sectores(santiago, [centro_santiago]).
 sectores(puerto_plata, [sosua]).
 sectores(higuey,[bavaro, punta_cana]).
-sectores(la_vega, [centro_la_vega]).
-sectores(samana, [las_galeras,las_terrenas]).
 
 %1.D sectores-actividades
-actividades(centro_santo_domingo, [discoteca1, discoteca2], [restaurante1, restaurante2, restaurante3], [cine1, cine2], [teatro1]).
-actividades(boca_chica, [discoteca1], [restaurante1, restaurante2], [], []).
-actividades(centro_santiago, [discoteca1, discoteca3, discoteca2], [restaurante1, restaurante2, restaurante3], [cine1, cine2], [teatro1]).
-actividades(sosua,[discoteca1, discoteca2], [restaurante1, restaurante2], [], []).
-actividades(punta_cana,[discoteca1,discoteca2], [restaurante1, restaurante2], [], []).
-actividades(centro_la_vega,[discoteca1, discoteca2], [restaurante1, restaurante2, restaurante3],[], []).
-actividades(las_galeras, [discoteca1], [restaurante1, restaurante2], [], []).
-actividades(las_terrenas, [discoteca2], [restaurante2, restaurante3], [], []).
-%1.F provincias-vecinas
-provincias_vecinas(santiago, la_vega).
-provincias_vecinas(santo_domingo,samana).
-provincias_vecinas(la_vega, samana).
+restaurante(restaurante1,boca_chica, 400, baja).
+restaurante(restaurante2,centro_santo_domingo, 500, normal).
+restaurante(restaurante3,centro_santiago, 1000, normal).
+restaurante(restaurante4,centro_santiago, 1400, alta).
+restaurante(restaurante5,sosua, 100, normal).
+restaurante(restaurante6,bavaro, 200, baja).
+restaurante(restaurante6,bavaro, 900, alta).
+restaurante(restaurante6,bavaro, 700, normal).
+restaurante(restaurante7,punta_cana, 250, normal).
+restaurante(restaurante7,punta_cana, 400, alta).
+
+discoteca(discoteca1,centro_santo_domingo, 100, baja).
+discoteca(discoteca2,centro_santiago, 500, normal).
+discoteca(discoteca3,centro_santiago, 500, alta).
+discoteca(discoteca4,sosua, 1200, alta).
+discoteca(discoteca5,bavaro, 1200, baja).
+discoteca(discoteca6,punta_cana, 1200, alta).
+discoteca(discoteca7, centro_santiago, 1400, alta).
+discoteca(discoteca8,punta_cana, 1200, alta).
+
+cine(cine1, centro_santiago, 500, normal).
+cine(cine2,centro_santiago, 250, alta).
+cine(cine3,centro_santiago, 250, alta).
+cine(cine4,centro_santo_domingo, 400, baja).
+cine(cine5,centro_santo_domingo, 300, alta).
+cine(cine6,centro_santo_domingo, 300, normal).
+cine(cine7,punta_cana, 250, alta).
+
 %2 tipocomida restaurantes
-tipocomida(restaurante1, [criolla, rapida, gourmet]).
-tipocomida(restaurante2, [gourmet, casera]).
-tipocomida(restaurante3, [criolla]).
+tipocomida(restaurante1, criolla).
+tipocomida(restaurante2, casera).
+tipocomida(restaurante3, casera).
+tipocomida(restaurante4, gourmet).
+tipocomida(restaurante5, buffet).
+tipocomida(restaurante6, criolla).
+tipocomida(restaurante7, rapida).
+
 %3 Rango de precio (economico, medio, elevado)
-rango_precio(0, 599, economico).
-rango_precio(600,1199, medio).
-rango_precio(1200,2400, elevado).
+rango_precio(Precio, economico):- Precio =< 599.
+rango_precio(Precio, medio):- Precio >= 600, Precio =< 1199.
+rango_precio(Precio, elevado):- Precio >= 1200.
 %4 playas
 playa(boca_chica).
 playa(sosua).
@@ -59,50 +78,27 @@ sector(punta_cana).
 sector(las_terrenas).
 sector(las_galeras).
 %6 eventos, sector
-eventos_importantes(concierto1, boca_chica).
-eventos_importantes(concierto2, sosua).
-eventos_importantes(carnaval, centro_la_vega).
+eventos_importantes(concierto1, 600, boca_chica).
+eventos_importantes(concierto2, 1300, sosua).
+eventos_importantes(carnaval, 0,centro_la_vega).
 %8 cine-peliculas
-cine(cine1, [pelicula1, pelicula2, pelicula3]).
-cine(cine2, [pelicula2,pelicula4, pelicula1]).
+cine(cine1, centro_santiago, [pelicula1, pelicula2, pelicula3, pelicula4]).
+cine(cine2, centro_santo_domingo, [pelicula2,pelicula4, pelicula1]).
+cine(cine3, centro_santo_domingo, [pelicula1, pelicula2, pelicula3, pelicula4]).
+cine(cine4, centro_santiago, [pelicula2,pelicula4, pelicula1]).
+cine(cine5, bavaro, [pelicula1, pelicula2, pelicula3]).
+cine(cine6, sosua, [pelicula2,pelicula4, pelicula1]).
+cine(cine7, centro_santiago,[pelicula2,pelicula4, pelicula1]).
 %8 A genero-pelicula
 genero(pelicula1, terror).
 genero(pelicula2, drama).
 genero(pelicula3, ficcion).
 genero(pelicula4, humor).
 
-%9 puntuacion-actividades
-puntuacion(discoteca1, mala).
-puntuacion(discoteca2, excelente).
-puntuacion(restaurante1, regular).
-puntuacion(restaurante2, regular).
-puntuacion(restaurante3, excelente).
-puntuacion(cine1, excelente).
-puntuacion(cine2, regular).
-puntuacion(teatro1, mala).
-
-%10 precioTodo 
-precio(concierto1, 600).
-precio(concierto2,1300).
-precio(carnaval, 0).
-precio(discoteca1, 500).
-precio(discoteca2, 1200).
-precio(restaurante1, 1250).
-precio(restaurante2, 700).
-precio(restaurante3, 200).
-precio(cine1, 250).
-precio(cine2, 300).
-precio(teatro1, 200).
-
 %reglas generales
-calidadActividadPrecio(PrecioActividad, Clasificacion):- rango_precio(Min, Max, Clasi),PrecioActividad >= Min, PrecioActividad =< Max, Clasificacion = Clasi, !. 
-
 buscarEnLista([], _):-fail.
 buscarEnLista([Cabeza|_], CasoABuscar):- CasoABuscar =:= Cabeza, !.
 buscarEnLista([_|Cola], CasoABuscar):-buscarEnLista(Cola, CasoABuscar).
-
-
-
 %reglas restaurante
 
 
@@ -115,11 +111,17 @@ buscarPeliculaPorGenero([], _):- fail.
 buscarPeliculaPorGenero([Cabeza|_], Genero ):- genero(Cabeza, Genero), !.
 buscarPeliculaPorGenero([_|Cola], Genero):- buscarPeliculaPorGenero(Cola, Genero).
 
-tienePelicula([], _, _):- fail.
-tienePelicula([Cabeza|_], Genero, Cine):- cine(Cabeza,Peliculas), buscarPeliculaPorGenero(Peliculas, Genero), Cine = Cabeza, !.
-tienePelicula([_|Cola], Genero, Cine):- tienePelicula(Cola,Genero, Cine).
+cinesPelicula(Lugar, Genero, Cines):- findall(Cine, (cine(Cine, Lugar, Peliculas), buscarPeliculaPorGenero(Peliculas, Genero)), Cines).
 
-cinePelicula(Lugar, Genero, Cine):- actividades(Lugar,_,_,Cines,_), tienePelicula(Cines, Genero, Cine).
 
+
+%Bares discotecas de precio elevado
+
+buscarDiscoPresupuesto(Lugar, Presupuesto, Puntuacion, CalPrecio, Discotecas):-findall(Disco, (
+    discoteca(Disco, Lugar, Precio, Puntuacion), rango_precio(Precio, CalPrecio), Precio =< Presupuesto), Discotecas).
+
+%discotecaPornormalPrecio([Cabeza|_], Puntuacion, Precio, Discotecas):- 
+
+%getDiscoteca(Precio, Puntuacion, Ciudad, Discotecas):-actividades(Ciudad,Discos_ciudad, _,_,_).
 
 
