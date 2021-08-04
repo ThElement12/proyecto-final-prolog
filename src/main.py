aa7a6dcfd4c61df52cdbf7a0128ceba78812c2e5
ThElement12 <20170861@ce.pucmm.edu.do>
2021-08-04 07:21:41 -0400

    Se sube ejemplo para correr Query

--- src/main.py
+++ src/main.py
@@ -1,24 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from pyswip import Prolog
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main_prueba():
     prolog = Prolog()
-    prolog.assertz("father(michael,john)")
-    prolog.assertz("father(michael,gina)")
-    list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
-    for soln in prolog.query("father(X,Y)"):
-        print(soln["X"], "is the father of", soln["Y"])
+    prolog.consult("rules_prolog/turista.pl")
+
+    query = "restaurantePresupuesto(5000, bavaro, medio, rapida, X)"
+    q2 = prolog.query(query)
+
+    for result in q2:
+        print(result)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main_prueba()
